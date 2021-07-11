99c8077018a869a8c4143c54d6e6f7402e665dfb
rahul <rahulrao0209@gmail.com>
2021-07-11 12:37:04 +0530

    initial-commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Navbar.css';
+import { SiCoffeescript } from 'react-icons/si';
+import { Sidebar } from '../sidebar/Sidebar';
+
+export const Navbar = () => {
+    const [sidebarIsOpen, setSidebarIsOpen] = React.useState(false);
+
+    return (
+        <>
+            <div className="navbar-container"
+             onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
+                <div className="navbar-wrapper">
+                    <div className="navbar-logo">
+                        Cafferina
+                    </div>
+                    <div className="sidebar-menu-icon"
+                    style={ sidebarIsOpen ? {opacity: 0} : {opacity: 1}}
+                    >
+                        <SiCoffeescript />
+                    </div>
+                    <Sidebar sidebarIsOpen={sidebarIsOpen}/>
+                    <ul className="navbar-links">
+                        <li className="navbar-link">
+                            <a href="#search">Search</a>
+                        </li>
+                        <li className="navbar-link">
+                            <a href="#cart">Cart</a>
+                        </li><li className="navbar-link">
+                            <a href="#order-now">Order Now</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/sidebar/sidebarStyleOptions.js
@@ -0,0 +1,11 @@
+export const sidebarOpenStyle = {
+    opacity: '100%',
+    top: '60px',
+    transition: 'all 0.4s ease',
+}
+
+export const sidebarCloseStyle = {
+    opacity: '0',
+    top: '-100%',
+    transition: 'all 0.4s ease'
+}
