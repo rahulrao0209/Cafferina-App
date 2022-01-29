a18c32f3dd3e09c6e2bc7e11cc08e5a0e93f9685
unknown <rahulrao0209@gmail.com>
2022-01-29 16:29:12 +0530

    fixed responsive layout bugs

--- src/components/home/heroSection/Hero.js
+++ src/components/home/heroSection/Hero.js
@@ -5,29 +5,32 @@ export const Hero = () => {
   return (
     <>
       <section className="hero-section">
+
         <div className="hero-container">
-          <div className="hero-content">
-            <div className="hero-image">
-              <img
-                src="./assets/images/hero-section/coffee-hero.png"
-                alt="coffee-mug"
-              />
-            </div>
-            <div className="hero-title">
+          
+          <div className="hero-image">
+             <img
+               src="./assets/images/hero-section/coffee-hero.png"
+               alt="coffee-mug"
+            />
+          </div>
+
+          <div className="hero-title">
               <p>
                 <span>Coffees And Snacks</span> for you
+                <p className="hero-title--info">
+                  We make sure everything we do honors that connection – from our
+                  commitment to the highest quality coffee in the world, to the
+                  way we engage with our customers and communities to do business
+                  responsibly.
+                </p>
               </p>
-              <p className="hero-title--info">
-                We make sure everything we do honors that connection – from our
-                commitment to the highest quality coffee in the world, to the
-                way we engage with our customers and communities to do business
-                responsibly.
-              </p>
-            </div>
+              <div className="hero-button">
+            <Link to="/order">
+              <div>Order Now</div>
+            </Link>
           </div>
-          <Link className="hero-button" to="/order">
-            <div>Order Now</div>
-          </Link>
+          </div>
         </div>
       </section>
     </>
